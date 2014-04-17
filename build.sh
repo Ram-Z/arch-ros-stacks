4279a381a60dacba7adf1b654679702944b80013
Samir Benmendil <samir.benmendil@gmail.com>
2014-04-17 05:55:54 +0100

    build.sh: add some output and install built packages

--- build.sh
+++ build.sh
@@ -25,6 +25,14 @@ get_pkgdest()
     echo "$PKGDEST"
 }
 
+msg() {
+    local mesg=$1; shift
+    local ALL_OFF="$(tput sgr0)"
+    local BOLD="$(tput bold)"
+    local GREEN="${BOLD}$(tput setaf 2)"
+    printf "${GREEN}==>${ALL_OFF}${BOLD} ${mesg}${ALL_OFF}\n" "$@" >&2
+}
+
 tmp=$(mktemp)
 dir=${1-:.}
 pkgbuilds=$(find "$dir" -name PKGBUILD)
@@ -45,9 +53,16 @@ for pkgname in ${sorted[@]}; do
     ver=$(get_version ./PKGBUILD)
     pkgdest=$(get_pkgdest)
     retcode=0
-    if [[ ! -e ignore ]] \
-        && test -z $(find $pkgdest -maxdepth 1 -name "$pkgname-*$ver*" -print -quit); then
-        makepkg -si --asdeps --noconfirm
+    if [[ ! -e ignore ]]; then
+        if [[ "$pkgname $ver" == "$(pacman -Q $pkgname 2>/dev/null)" ]]; then
+            msg "$pkgname-$ver uptodate!"
+        elif [[ -z $(find $pkgdest -maxdepth 1 -name "$pkgname-*$ver*" -print -quit) ]]; then
+            msg "Building $pkgname"
+            makepkg -si --asdeps --noconfirm
+        elif [[ -z $(pacman -Qq $pkgname 2> /dev/null) ]]; then
+            msg "Installing $pkgname"
+            sudo pacman -U --noconfirm --asdeps *.pkg.tar.xz
+        fi
         retcode=$?
     fi
     popd > /dev/null
